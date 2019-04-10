99289b4dbb4e245734db494a37cc5e1c463a1760
ellaneurohr <elianajneu@gmail.com>
2019-04-10 15:55:52 -0600

    Made choose game method and put in switch statement for 1 game

--- src/GameFactory.java
+++ src/GameFactory.java
@@ -2,5 +2,13 @@
  * Class for the factory that switches to the user-selected game
  */
 public class GameFactory {
-	
+
+    public Game selectGame(int userChoice) {
+        switch (userChoice) {
+            case 1:
+                return new RandomScore();
+            default:
+                return null;
+        }
+    }
 }
