7547841041f543089a0590e90e60c7266d30ed33
Will-pasley <49408211+Will-pasley@users.noreply.github.com>
2019-04-10 18:12:42 -0600

    Here's good things

---
+++ src/Package/ArcadeMenu.java
@@ -0,0 +1,30 @@
+package Package;
+import java.io.IOException;
+import com.sun.tools.javac.Main;
+
+import javafx.application.*;
+import javafx.stage.*;
+import javafx.fxml.*;
+import javafx.scene.*;
+import javafx.scene.layout.*;
+
+public class ArcadeMenu extends Application {
+
+	private Stage primaryStage;
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception{
+		this.primaryStage = primaryStage;
+	
+		Parent root = FXMLLoader.load(getClass().getResource("Menu.fxml"));
+		primaryStage.setTitle("Menu");
+		primaryStage.setScene(new Scene(root, 600,700));
+		primaryStage.show();
+		
+	}
+	
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
---
+++ src/Package/MenuController.java
@@ -0,0 +1,11 @@
+package Package;
+public class MenuController {
+	
+	private ArcadeMenu arcadeMenu;
+	
+	public void setArcadeMenu(ArcadeMenu arcadeMenu)
+	{
+		this.arcadeMenu = arcadeMenu;
+	}
+
+}
