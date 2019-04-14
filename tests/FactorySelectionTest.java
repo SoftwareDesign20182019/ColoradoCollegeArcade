98268205eb3f96071536c5aed9700267349eda76
hrose <h_rose@coloradocollege.edu>
2019-04-14 16:56:37 -0600

    new tests started

--- tests/FactorySelectionTest.java
+++ tests/FactorySelectionTest.java
@@ -1,12 +1,16 @@
 import static org.junit.Assert.*;
-
 import org.junit.Before;
 import org.junit.Test;
+import Package.GameFactory;
+import Package.Arcade;
 
 public class FactorySelectionTest {
 
+	private GameFactory factory;
+	
 	@Before
 	public void setUp() throws Exception {
+		factory = new GameFactory();
 	}
 
 	@Test
