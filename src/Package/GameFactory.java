f83e30db24497a220389ffd444d057793e6c0f39
hrose <h_rose@coloradocollege.edu>
2019-04-14 18:39:40 -0600

    new final static ints

--- src/Package/GameFactory.java
+++ src/Package/GameFactory.java
@@ -15,23 +15,16 @@ public class GameFactory {
 
     public Game selectGame(int userChoice) throws Exception {
         switch (userChoice) {
-            case 1:
-                RandomScore randomScore = new RandomScore();
-                return randomScore;
-            case 2:
+            case Arcade.JAVELIN_THROW:
             	JavelinThrow javelinGame = new JavelinThrow();
-            	/*
-            	FXMLLoader loader = new FXMLLoader(getClass().getResource("Javelin.fxml"));
-        		Parent javelinRoot = loader.load();
-        		Stage javelinStage = new Stage();
-        		javelinStage.setTitle("Menu");
-        		javelinStage.setScene(new Scene(javelinRoot, 800,400));
-        		javelinStage.show();
-        		*/
-            	return javelinGame;
-            case 4:
+                return javelinGame;
+            case Arcade.RECTANGLE_MAN:
                 RectangleManMain rectangleManMain = new RectangleManMain();
                 return rectangleManMain;
+            case Arcade.SUPER_VACUUM:
+            	return null;
+            case Arcade.CLICKER_GAME:
+            	return null;
             default:
                 return null;
         }
