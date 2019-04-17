78f95bb0a2b2541f9b95ff4c2ab3e62d6f167de7
hrose <h_rose@coloradocollege.edu>
2019-04-16 22:23:07 -0600

    commit for merge

--- src/Package/ClickerController.java
+++ src/Package/ClickerController.java
@@ -1,5 +1,7 @@
 package Package;
 
+
+
 import java.awt.*;
 import java.net.URL;
 import java.util.ResourceBundle;
--- src/Package/ClickerIdle.java
+++ src/Package/ClickerIdle.java
@@ -1,12 +1,14 @@
 package Package;
 
+
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.util.Random;
+import java.util.Randoe;
 
 public class ClickerIdle extends Application implements Game {
 
